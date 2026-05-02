331aed27ed3b29b38f752fe31af344a126509736
palak-commit <palakpatel00132@gmail.com>
2026-05-02 18:49:42 +0530

    Add root / route to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
 app.use("/api", apiRoutes);
 
 app.use((req, res) => {
