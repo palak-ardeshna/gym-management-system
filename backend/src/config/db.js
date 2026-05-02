e7c85fd81dee15ed5492ebefb5ac5f72ecf738b8
palak-commit <palakpatel00132@gmail.com>
2026-05-02 18:42:57 +0530

    Fix: Add pg dialectModule and SSL options for Vercel/Live DB deployment

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,10 +1,18 @@
 import { Sequelize } from "sequelize";
+import pg from "pg";
 import { env } from "./env.js";
 
 export const sequelize = new Sequelize(env.dbName, env.dbUser, env.dbPassword, {
   host: env.dbHost,
   port: env.dbPort,
   dialect: "postgres",
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
   logging: false,
 });
 
